fa1f6ea4b6cbed27521d3c70d76472b2d3b63d8f
Amit Verma <144882964+AmitVerma-01@users.noreply.github.com>
2023-09-25 18:02:29 +0530

    Update 141. Linked List Cycle.cpp

--- linked-list/141. Linked List Cycle.cpp
+++ linked-list/141. Linked List Cycle.cpp
@@ -1,3 +1,4 @@
+// brute force approach using hashing TC-O(N) & SC-O(N)
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
@@ -9,5 +10,22 @@ public:
             head=head->next;
         }
     return false;
+    }
+};
+// Optimal Approach using two pointer (slow and fasr pointer 
+//  TC-O(N) & SC-O(1)
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=NULL && fast->next!=NULL){
+            if(fast==slow){
+                return true;
+            }
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return false;
     }
 };
