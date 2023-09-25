32ef00284f0fe55317a548e0d2af02bb245eaca4
Amit Verma <144882964+AmitVerma-01@users.noreply.github.com>
2023-09-25 18:05:08 +0530

    Update 141. Linked List Cycle.cpp

--- linked-list/141. Linked List Cycle.cpp
+++ linked-list/141. Linked List Cycle.cpp
@@ -20,11 +20,12 @@ public:
         ListNode* fast=head;
         ListNode* slow=head;
         while(fast!=NULL && fast->next!=NULL){
+           
+            fast=fast->next->next;
+            slow=slow->next;
             if(fast==slow){
                 return true;
             }
-            fast=fast->next->next;
-            slow=slow->next;
         }
         return false;
     }
